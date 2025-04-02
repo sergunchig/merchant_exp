23a09285b2fbd418a5173c13e1d83090253a702c
sergunchig <sergunchig@yandex.ru>
2025-04-02 22:34:01 +0300

    refactoring add dto (not finished)

--- internal/repo/offer_postgres.go
+++ internal/repo/offer_postgres.go
@@ -71,7 +71,7 @@ func (r *OfferRepo) CreateOffersPipe(ctx context.Context, in <-chan entity.Offer
 	return nil
 }
 
-func (r *OfferRepo) GetOffers(ctx context.Context) ([]entity.Offer, error) {
+func (r *OfferRepo) Read(ctx context.Context) ([]entity.Offer, error) {
 	query := "select offer_id, \"name\", price, available  from offers"
 
 	rows, err := r.client.Pool.Query(ctx, query)
@@ -92,6 +92,32 @@ func (r *OfferRepo) GetOffers(ctx context.Context) ([]entity.Offer, error) {
 
 	return offers, nil
 }
+
+func (r *OfferRepo) ReadAsync(ctx context.Context) (chan<- entity.Offer, chan<- error) {
+	in := make(chan entity.Offer)
+	errCh := make(chan error)
+
+	query := "select offer_id, \"name\", price, available  from offers"
+
+	go func() {
+		rows, err := r.client.Pool.Query(ctx, query)
+		if err != nil {
+			errCh <- fmt.Errorf("error query %w", err)
+		}
+		defer rows.Close()
+
+		for rows.Next() {
+			var offer entity.Offer
+			if err := rows.Scan(&offer.OfferId, &offer.Name, &offer.Price, &offer.Available); err != nil {
+				errCh <- fmt.Errorf("error row scan %w", err)
+			}
+			in <- offer
+		}
+	}()
+
+	return in, errCh
+}
+
 func (r *OfferRepo) GetOffer(offer_id int) (entity.Offer, error) {
 	return entity.Offer{}, nil
 }
